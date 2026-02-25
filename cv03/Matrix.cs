e2acc4d9c5354743cb55be5059f7cc08e93ec26d
Pisky02 <114362821+Pisky02@users.noreply.github.com>
2026-02-25 17:45:00 +0100

    cv03 implikace operac9 s maticemi

---
+++ cv03/Matrix.cs
@@ -0,0 +1,174 @@
+﻿/*
+BPC-OOP cv3
+
+1. Vytvořte nový projekt podle šablony „Console App“. Target Framework zvolte .NET 8.0.
+2. Vytvořte vlastní třídu Matrix pro práci s maticemi. Hodnoty v matici budou typu double.
+    Vnitřně matici ukládejte do datového členu, který bude typu double[,]. Vytvořte konstruktor
+    s jedním argumentem typu dvourozměrné pole double[,].
+3. Implementujte operátory +, -, *, ==, !=, unární operátor -. Implementujte metodu ToString
+    pro zobrazení matice na konzoli. Napište metodu vracející determinant matice pro rozměry
+    do velikosti 3x3. Chybové stavy (např. nekompatibilita matic při operacích, u determinantu
+    pokud není matice čtvercová či je větší než 3x3) ošetřete výjimkami.
+4. V metodě Main provolejte jednotlivé operátory a metody a s využitím Matlabu ověřte
+    správnost výpočtů v těchto operátorech a metodách 
+ */
+
+internal class Matrix
+{ 
+    private double[,] matrix;
+
+    public Matrix(double[,] matrix)
+    {
+        this.matrix = matrix;
+    }
+
+    private int Rows
+    {
+        get { return matrix.GetLength(0); }
+    }
+    private int Cols
+    {
+        get { return matrix.GetLength(1); }
+    }
+
+    //Operatory +, -, *, ==, !=, unární operator -
+    public static Matrix operator +(Matrix a, Matrix b)
+    {
+        if(a.Rows != b.Rows || a.Cols != b.Cols)
+        {
+            throw new Exception("Nekompatibilní rozměry matic pro sčítání.");
+        }
+        double[,] result = new double[a.Rows, a.Cols];
+        for(int rowIndex = 0; rowIndex < a.Rows; rowIndex++)
+        {
+            for(int colIndex = 0; colIndex < a.Cols; colIndex++)
+            {
+                result[rowIndex, colIndex] = a.matrix[rowIndex, colIndex] + b.matrix[rowIndex, colIndex];
+            }
+        }
+        return new Matrix(result);
+    }
+
+    public static Matrix operator -(Matrix a, Matrix b)
+    {
+        if(a.Rows != b.Rows || a.Cols != b.Cols)
+        {
+            throw new Exception("Nekompatibilní rozměry matic pro odčítání.");
+        }
+        double[,] result = new double[a.Rows, a.Cols];
+        for(int rowIndex = 0; rowIndex < a.Rows; rowIndex++)
+        {
+            for(int colIndex = 0; colIndex < a.Cols; colIndex++)
+            {
+                result[rowIndex, colIndex] = a.matrix[rowIndex, colIndex] - b.matrix[rowIndex, colIndex];
+            }
+        }
+        return new Matrix(result);
+    }
+    public static Matrix operator *(Matrix a, Matrix b)
+    {
+        if(a.Cols != b.Rows)
+        {
+            throw new Exception("Nekompatibilní rozměry matic pro násobení.");
+        }
+
+        double[,] result = new double[a.Rows, b.Cols];
+
+        for(int rowIndex = 0; rowIndex < a.Rows; rowIndex++)
+        {
+            for(int colIndex = 0; colIndex < b.Cols; colIndex++)
+            {
+                double sum = 0;
+                for(int sumIndex = 0; sumIndex < a.Cols; sumIndex++)
+                {
+                    sum += a.matrix[rowIndex, sumIndex] * b.matrix[sumIndex, colIndex];
+                }
+
+                result[rowIndex, colIndex] = sum;
+            }
+        }
+        return new Matrix(result);
+    }
+
+    public static bool operator ==(Matrix a, Matrix b)
+    {
+        if(a.Rows != b.Rows || a.Cols != b.Cols)
+        {
+            return false;
+        }
+        for(int rowIndex = 0; rowIndex < a.Rows; rowIndex++)
+        {
+            for(int colIndex = 0; colIndex < a.Cols; colIndex++)
+            {
+                if(a.matrix[rowIndex, colIndex] != b.matrix[rowIndex, colIndex])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static bool operator !=(Matrix a, Matrix b)
+    {
+        return !(a == b);
+    }
+
+    public static Matrix operator -(Matrix a)
+    {
+        double[,] result = new double[a.Rows, a.Cols];
+        for(int rowIndex = 0; rowIndex < a.Rows; rowIndex++)
+        {
+            for(int colIndex = 0; colIndex < a.Cols; colIndex++)
+            {
+                result[rowIndex, colIndex] = -a.matrix[rowIndex, colIndex];
+            }
+        }
+        return new Matrix(result);
+    }
+
+    //ToString pro zobrazení matice na konzoli
+    public override string ToString()
+    {
+        string result = "";
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Cols; j++)
+            {
+                // result += string.Format("{0,10:F2}", matrix1[i, j]);
+                result += $"{matrix[i, j], 10:F2}";
+            }
+            result += Environment.NewLine;
+        }
+        return result;
+    }
+
+    //Metoda vracející determinant matice pro rozměry do velikosti 3x3
+    public double Determinant()
+    {
+        if(Rows != Cols)
+        {
+            throw new Exception("Matice není čtvercová.");
+        }
+        if(Rows > 3)
+        {
+            throw new Exception("Matice může být nanejvýš rozměru 3x3.");
+        }
+        if(Rows == 1)
+        {
+            return matrix[0, 0];
+        }
+        else if(Rows == 2)
+        {
+            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+        }
+        else 
+        {
+            double a = matrix[0, 0], b = matrix[0, 1], c = matrix[0, 2];
+            double d = matrix[1, 0], e = matrix[1, 1], f = matrix[1, 2];
+            double g = matrix[2, 0], h = matrix[2, 1], i = matrix[2, 2];
+            return (a*e*i + d*h*c + g*b*f) - (g*e*c + a*f*h + b*d*i);
+        }
+    
+    }
+}
---
+++ cv03/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+BPC-OOP cv3
+
+1. Vytvořte nový projekt podle šablony „Console App“. Target Framework zvolte .NET 8.0.
+2. Vytvořte vlastní třídu Matrix pro práci s maticemi. Hodnoty v matici budou typu double.
+    Vnitřně matici ukládejte do datového členu, který bude typu double[,]. Vytvořte konstruktor
+    s jedním argumentem typu dvourozměrné pole double[,].
+3. Implementujte operátory +, -, *, ==, !=, unární operátor -. Implementujte metodu ToString
+    pro zobrazení matice na konzoli. Napište metodu vracející determinant matice pro rozměry
+    do velikosti 3x3. Chybové stavy (např. nekompatibilita matic při operacích, u determinantu
+    pokud není matice čtvercová či je větší než 3x3) ošetřete výjimkami.
+4. V metodě Main provolejte jednotlivé operátory a metody a s využitím Matlabu ověřte
+    správnost výpočtů v těchto operátorech a metodách 
+ */
+
+Matrix matrixA = new Matrix(new double[,] { { 1, -2 ,5 }, { -3, 4, 8  }, { 2, 5, 9 } });
+Matrix matrixB = new Matrix(new double [,] { { 5, 6 }, { 7, 8 } });
+Matrix matrixC = new Matrix(new double [,] { { 8 } });
+Matrix matrixD = new Matrix(new double [,] { { 5, 6, 9, 6 }, { 7, 8, 10 , 2}, { 5, 6, 9, 6 }, { 7, 8, 10, 2 }});
+
+Matrix matrixE = new Matrix(new double [,] { { 5, 6, -9 }, { 7, 2, 10 }, { 4, 6, 8} });
+Matrix matrixF = new Matrix(new double [,] { { 7, 1 }, { -8, 10 } });
+Matrix matrixG = new Matrix(new double [,] { { 5 } });
+Matrix matrixH = new Matrix(new double [,] { { 5, 2 }, { 7, 4 }, { 5, 6} });
+
+Console.WriteLine("Matrix A + Matrix E (3x3): \n{0}", matrixA + matrixE);
+Console.WriteLine("Matrix B - Matrix F (2x2): \n{0}", matrixB - matrixF);
+Console.WriteLine("Matrix A * Matrix E (3x3): \n{0}", matrixA * matrixE);
+Console.WriteLine("Matrix C == Matrix C (1x1): \n{0}", matrixC == matrixC);
+Console.WriteLine("Matrix C != Matrix G (1x1): \n{0}", matrixC != matrixG);
+Console.WriteLine("-Matrix A: \n{0}", -matrixA);
+Console.WriteLine("Determinant of Matrix A (3x3): \n{0}", matrixA.Determinant());
+Console.WriteLine("Determinant of Matrix B (2x2): \n{0}", matrixB.Determinant());
+Console.WriteLine("Determinant of Matrix C (1x1): \n{0}", matrixC.Determinant());
+
+try
+{
+ 
+    Console.WriteLine("Determinant of Matrix H (2x3): \n{0}", matrixH.Determinant());
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Chyba: {0}", ex.Message);
+}
+try
+{
+
+    Console.WriteLine("Determinant of Matrix D (4x4): \n{0}", matrixD.Determinant());
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Chyba: {0}", ex.Message);
+}
+try
+{
+
+    Console.WriteLine("Matrix A * Matrix B (3x3): \n{0}", matrixA * matrixB);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Chyba: {0}", ex.Message);
+}
+try
+{
+
+    Console.WriteLine("Matrix B + Matrix E (3x3): \n{0}", matrixB + matrixE);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Chyba: {0}", ex.Message);
+}
+
+
+
+
+
+
